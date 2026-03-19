fd314d54445a1ab8727ee344a0d81b99628b5ef2
KevinMargason <s160423014@student.ubaya.ac.id>
2026-03-19 21:31:21 +0700

    Bisa amin

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -28,7 +28,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://easykos.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
