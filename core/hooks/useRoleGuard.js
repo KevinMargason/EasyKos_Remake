779e7d7e23e4a034c9fa6ba9c60aaecd3c628587
KevinMargason <s160423014@student.ubaya.ac.id>
2026-03-20 17:06:18 +0700

    Update useRoleGuard.js

--- core/hooks/useRoleGuard.js
+++ core/hooks/useRoleGuard.js
@@ -5,11 +5,21 @@ import { usePathname, useRouter } from "next/navigation";
 import { useAppSelector } from "@/core/store/hooks";
 import { ROLE_HOME, ROUTES, normalizeRole } from "@/lib/routes";
 
+/**
+ * @typedef {Object} UseRoleGuardOptions
+ * @property {Array<'user' | 'owner'>=} allowedRoles
+ * @property {string=} fallbackWhenNoRole
+ * @property {Record<'user' | 'owner', string>=} roleHome
+ */
+
+/**
+ * @param {UseRoleGuardOptions} [options]
+ */
 export const useRoleGuard = ({
     allowedRoles,
     fallbackWhenNoRole = ROUTES.LOGIN,
     roleHome = ROLE_HOME,
-} = {}) => {
+} = /** @type {UseRoleGuardOptions} */ ({})) => {
     const router = useRouter();
     const pathname = usePathname();
     const roleFromStore = useAppSelector((state) => state.role.role);
